36c99ce02c788c3f60e470721ddab7a6f4bd29d2
Haoyi Gao <gao.haoy@northeastern.edu>
2024-11-12 17:12:39 -0800

    updated event component to read from Event collection

--- Screen/Event.js
+++ Screen/Event.js
@@ -1,19 +1,40 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import Color from '../Style/Color'
+import { Text, View } from 'react-native'
+import React, { useEffect, useState } from 'react'
+import { globalStyles } from '../Style/Styles'
+import { fetchAllDocuments } from '../Firebase/firebaseHelper'
 
-export default function Event() {
+export default function Event({navigation}) {
+  const [events, setEvents] = useState('')
+  const [loading, setLoading] = useState(true)
+  
+  async function fetchEvents() {
+    try {
+      const data = await fetchAllDocuments('Event')
+      setEvents(data)
+      console.log('Events:', data)
+      setLoading(false)
+    } catch (error) {
+      console.log('Error fetching events:', error)
+    }
+  }
+
+  useEffect(() => {
+    const unsubscribe = navigation.addListener('focus', () => {
+      fetchEvents()
+    })
+    return unsubscribe
+  }, [navigation])
+
   return (
-    <View style={styles.container}>
-      <Text>Event</Text>
+    <View style={globalStyles.container}>
+      {loading ? (
+        <Text>Loading...</Text>
+      ) : (
+        // displaying successful reads from Event firebase collection
+        events.map((event) => (
+          <Text key={event.id}>{event.title}</Text>
+        ))
+      )}
     </View>
   )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: Color.pageBackground,
-
-  },
-})
