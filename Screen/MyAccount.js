0599916711b586d543d0195484f9eeb0a940b4fa
Jie <jiedeng1113@outlook.com>
2024-11-23 00:41:31 -0800

    updated myaccount, added static map to show user's current location and also allow user to select a location in an interactive map

--- Screen/MyAccount.js
+++ Screen/MyAccount.js
@@ -1,30 +1,39 @@
-import { Text, View ,StyleSheet,Image} from 'react-native'
-import React, { useEffect, useState } from 'react'
-import { getAuth } from 'firebase/auth'
-import { globalStyles } from '../Style/Styles'
-import { signOut } from 'firebase/auth'
-import PressableItem from '../Component/PressableItem'
-import FontAwesome from 'react-native-vector-icons/FontAwesome'
-import Color from '../Style/Color'
-import LocationManager from '../Component/LocationManager'
+// 
 
-export default function MyAccount({navigation}) {
-  const [user, setUser] = useState(null)
-  const auth = getAuth()
-  
+import React, { useEffect, useState } from "react";
+import { View, Text, StyleSheet, Image } from "react-native";
+import { getAuth, signOut } from "firebase/auth";
+import { getItem } from "../Firebase/firebaseHelper"; // Import the helper function
+import { globalStyles } from "../Style/Styles";
+import PressableItem from "../Component/PressableItem";
+import FontAwesome from "react-native-vector-icons/FontAwesome";
+import Color from "../Style/Color";
+import LocationManager from "../Component/LocationManager";
+
+export default function MyAccount({ navigation }) {
+  const [user, setUser] = useState(null);
+  const auth = getAuth();
+
   useEffect(() => {
-    const currentUser = auth.currentUser
-    if (currentUser) {
-      setUser({
-        
-        email: currentUser.email,
-        uid: currentUser.uid,
-        name: currentUser.displayName,
-        // phoneNumber: currentUser.phoneNumber,
-        // photo: currentUser.photoURL
-      })
-    }
-  }, [auth])
+    const fetchUserData = async () => {
+      const currentUser = auth.currentUser;
+
+      if (currentUser) {
+        try {
+          const userData = await getItem("users", currentUser.uid); // Fetch user data from Firestore
+          setUser({
+            email: currentUser.email,
+            uid: currentUser.uid,
+            ...userData, // Merge Firestore data (e.g., userName, likedProducts, photoURI)
+          });
+        } catch (error) {
+          console.error("Error fetching user data:", error);
+        }
+      }
+    };
+
+    fetchUserData();
+  }, [auth]);
 
   return (
     <View style={globalStyles.formContainer}>
@@ -32,72 +41,60 @@ export default function MyAccount({navigation}) {
         <>
           <View style={styles.profileContainer}>
             {/* Avatar and User Info */}
-            <View style={{ flexDirection: 'row', alignItems: 'center', marginBottom: 20 }}>
+            <View style={{ flexDirection: "row", alignItems: "center", marginBottom: 20 }}>
               {/* Avatar */}
               <Image
                 source={
-                  user.photoURL
-                    ? { uri: user.photoURL }
-                    : require('../assets/notice.jpg') // Replace with the path to your default avatar image
+                  user.photoURI
+                    ? { uri: user.photoURI }
+                    : require("../assets/notice.jpg") // Replace with default avatar
                 }
                 style={{ width: 100, height: 100, borderRadius: 50, marginRight: 10 }}
               />
-              
+
               {/* User Details */}
               <View>
-                {user.name && <Text style={styles.userName}>{user.name}</Text>}
+                {user.userName && <Text style={styles.userName}>{user.userName}</Text>}
                 <Text style={styles.userEmail}>
-                <FontAwesome
-                  name={'envelope-o'}
-                  size={16}
-                  color={'black'}
-                  style={{ left: 10 }}
-                /> {user.email}</Text>
+                  <FontAwesome name="envelope-o" size={16} color="black" style={{ left: 10 }} />{" "}
+                  {user.email}
+                </Text>
               </View>
             </View>
 
+            {/* Profile Details */}
             <View style={styles.profileDetail}>
-  
-            {/* Liked Products */}
-            <PressableItem
-              pressedFunction={() => {
-                navigation.navigate('User Favorite', { type: 'Product', userId: user.uid });
-              }}
-              componentStyle={globalStyles.pressable}
-              pressedStyle={globalStyles.pressablePressed}
-            >
-              <View style={{ flexDirection: 'row', width: 160 }}>
-                <FontAwesome
-                  name={'heart-o'}
-                  size={16}
-                  color={'black'}
-                  style={{ left: 10 }}
-                />
-                <Text style={{ left: 30 }}>Liked Products</Text>
-              </View>
-            </PressableItem>
-  
-            {/* Interested Events */}
-            <PressableItem
-              pressedFunction={() => {
-                navigation.navigate('User Favorite', { type: 'Event', userId: user.uid });
-              }}
-              componentStyle={globalStyles.pressable}
-              pressedStyle={globalStyles.pressablePressed}
-            >
-              <View style={{ flexDirection: 'row', width: 160 }}>
-                <FontAwesome
-                  name={'star-o'}
-                  size={16}
-                  color={'black'}
-                  style={{ left: 10 }}
-                />
-                <Text style={{ left: 30 }}>Interested Events</Text>
-              </View>
-            </PressableItem>
-  
-            {/* My Listings */}
-            <PressableItem
+              {/* Liked Products */}
+              <PressableItem
+                pressedFunction={() =>
+                  navigation.navigate("User Favorite", { type: "Product", userId: user.uid })
+                }
+                componentStyle={globalStyles.pressable}
+                pressedStyle={globalStyles.pressablePressed}
+              >
+                <View style={{ flexDirection: "row", width: 160 }}>
+                  <FontAwesome name="heart-o" size={16} color="black" style={{ left: 10 }} />
+                  <Text style={{ left: 30 }}>Liked Products</Text>
+                </View>
+              </PressableItem>
+
+              {/* Interested Events */}
+              <PressableItem
+                pressedFunction={() =>
+                  navigation.navigate("User Favorite", { type: "Event", userId: user.uid })
+                }
+                componentStyle={globalStyles.pressable}
+                pressedStyle={globalStyles.pressablePressed}
+              > 
+                <View style={{ flexDirection: "row", width: 160 }}>
+                  <FontAwesome name="star-o" size={16} color="black" style={{ left: 10 }} />
+                  <Text style={{ left: 30 }}>Interested Events</Text>
+                </View>
+              </PressableItem>
+
+
+                {/* My Listings */}
+                <PressableItem
               pressedFunction={() => {
                 navigation.navigate('User Favorite', { type: 'Product', userId: user.uid, myListings: true });
               }}
@@ -133,7 +130,6 @@ export default function MyAccount({navigation}) {
                 <Text style={{ left: 30 }}>My reminders</Text>
               </View>
             </PressableItem>
-
             <View style={{ flexDirection: 'row', justifyContent: 'space-between', marginTop: 20 }}>
             {/* Edit Profile */}
             <PressableItem
@@ -165,8 +161,9 @@ export default function MyAccount({navigation}) {
               <Text style={{ color: 'white' }}>Logout</Text>
             </PressableItem>
           </View>
+            </View>
           </View>
-          </View>
+
           {/* LocationManager Section */}
           <View style={styles.locationContainer}>
             <LocationManager />
@@ -182,40 +179,40 @@ export default function MyAccount({navigation}) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'white',
+    backgroundColor: "white",
   },
   profileContainer: {
     flex: 6,
-    backgroundColor: 'white',
+    backgroundColor: "white",
     padding: 20,
     borderRadius: 10,
     marginBottom: 20,
-    shadowColor: 'black',
+    shadowColor: "black",
     shadowOffset: { width: 0, height: 2 },
   },
   profileDetail: {
     flex: 3,
-    backgroundColor: 'grey',
+    backgroundColor: "grey",
     padding: 20,
     borderRadius: 10,
-    shadowColor: 'black',
+    shadowColor: "black",
     shadowOffset: { width: 0, height: 2 },
   },
   userName: {
-    fontSize: 20,
-    fontWeight: 'bold',
+    fontSize: 25,
+    fontWeight: "bold",
   },
   userEmail: {
     fontSize: 14,
-    color: 'grey',
+    color: "grey",
   },
   locationContainer: {
     flex: 2,
-    backgroundColor: 'white',
+    backgroundColor: "white",
     padding: 20,
     borderRadius: 10,
     marginBottom: 20,
-    shadowColor: 'black',
+    shadowColor: "black",
     shadowOffset: { width: 0, height: 2 },
   },
 });
